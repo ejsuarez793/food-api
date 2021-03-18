cc383d43edc48ccb7dc1db51e9a71ff3bef718b6
ejsuarez793 <ejsuarez793@gmail.com>
2021-03-17 21:14:27 -0300

    change exception variable name

--- app/repositories/ingredients.py
+++ app/repositories/ingredients.py
@@ -42,15 +42,15 @@ def multiget(ids: List[int]) -> Union[List[Dict], Dict]:
 
     try:
         validated_ids = [int(id) for id in ids]
-    except Exception as e:
+    except Exception:
         log.debug('invalid params for ingredients multiget [ids:%s]',','.join(ids))
         return None, {'msg': 'invalid params for multiget', 'status_code': 400}
 
     try:
         ingredients = Ingredient.multiget(validated_ids)
-    except Exception as e:
+    except Exception as exception:
         log.error('there was a database error while getting ingredients [ids:%s][error:%s]',
-                  ','.join(validated_ids), str(e))
+                  ','.join(validated_ids), str(exception))
         return None, {'msg': 'there was an error getting ingredients', 'status_code': 500}
 
     return IngredientSchema(many=True).dump(ingredients), None
@@ -70,15 +70,15 @@ def create(data: Dict) -> Union[Dict, Dict]:
     try:
         validated_data = schema.load(data)
         new_ingredient = Ingredient(**validated_data)
-    except ValidationError as e:
-        log.debug('there was an error validating ingredient: [{error}]'.format(error=str(e)))
+    except ValidationError as exception:
+        log.debug('there was an error validating ingredient: [{error}]'.format(error=str(exception)))
         return None, {'msg': 'there was an error validating ingredient', 'status_code': 400}
 
     try:
         db.session.add(new_ingredient)
         db.session.commit()
-    except Exception as e:
-        log.debug('there was a database error creating ingredient', str(e))
+    except Exception as exception:
+        log.error('there was a database error creating ingredient [error:{}]'.format(str(exception)))
         return None, {'msg': 'there was an error creating ingredient', 'status_code': 500}
 
     return schema.dump(new_ingredient), None
@@ -95,8 +95,8 @@ def delete(id: int) -> Union[Dict, Dict]:
     # Todo: este validacion del .first() hace falta? o solo el .delete() ya esta
     try:
         recipe = Ingredient.query.filter(Ingredient.id == id).first()
-    except Exception as e:
-        log.error('there was a database error finding ingredient for deletion [id:%d]', id)
+    except Exception as exception:
+        log.error('there was a database error finding ingredient for deletion [id:%d][error:%s]',id, str(exception))
         return None, {'msg': 'there was an error deleting ingredient', 'status_code': 500}
 
     if not recipe:
@@ -105,8 +105,8 @@ def delete(id: int) -> Union[Dict, Dict]:
     try:
         Ingredient.query.filter(Ingredient.id == id).delete()
         db.session.commit()
-    except Exception as e:
-        log.error('there was a database error deleting ingredient [id:%d]', id)
+    except Exception as exception:
+        log.error('there was a database error deleting ingredient [id:%d][error:%s]', id, str(exception))
         return None, {'msg': 'there was an error deleting ingredient', 'status_code': 500}
 
     return {}, None
