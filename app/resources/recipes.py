d0ba8f63136f5329175e0f618d10543587ad3a13
ejsuarez793 <ejsuarez793@gmail.com>
2021-03-18 21:25:20 -0300

    routes.py and recipes.py style changes

--- app/resources/recipes.py
+++ app/resources/recipes.py
@@ -1,3 +1,12 @@
+"""
+Resources for recipes, basically like Resources controllers
+that communicates with repositories as needed
+
+Defines three recipe resource: Recipe, RecipeById and RecipeRecommendation
+just because method get needs to have different logic based on route
+and params
+"""
+
 from flask import request, jsonify, make_response
 from flask_restx import Resource
 from marshmallow import RAISE
@@ -14,16 +23,32 @@ rrqp = RecipesRecommendationsQueryParser()
 
 class Recipe(Resource):
 
-    @recipeSearchQueryParser.use_args(RecipeSearchQuery(unknown=RAISE), location='query')
+    @recipeSearchQueryParser.use_args(RecipeSearchQuery(unknown=RAISE),
+                                      location='query')
     def get(self, params: 'RecipeSearchQuery'):
+        """
+        Get recipes based of query params
+
+        :param params: RecipeSearchQuery object parsed from query params
+        :return: recipes result on succes, bad request or internal server
+        error otherwise
+        """
+
         try:
             return recipes.get_with_params(params)
         except ValueError:
-            return make_response(jsonify({'msg':'bad_request'}), 400)
+            return make_response(jsonify({'msg': 'bad_request'}),
+                                 400)
         except:
-            return make_response(jsonify({'msg':'internal_server_error'}), 500)
+            return make_response(jsonify({'msg': 'internal_server_error'}),
+                                 500)
 
     def post(self):
+        """
+        post method to create recipes
+        :return: newly created recipe on success, error otherwise
+        """
+
         json_data = request.get_json()
         response, err = recipes.create_recipe(json_data)
         if err:
@@ -33,20 +58,39 @@ class Recipe(Resource):
 
 class RecipeById(Resource):
 
-    def get(self, id: str):
-        return recipes.get_by_id(id)
+    def get(self, recipe_id: str):
+        """
+        Get recipe by id
+        :param recipe_id: recipe id uuid str
+        :return: recipe if found
+        """
 
-    def put(self, id: str):
+        return recipes.get_by_id(recipe_id)
+
+    def put(self, recipe_id: str):
+        """
+        Put recipe by id
+        :param recipe_id: recipe_id uuid str
+        :return: updated recipe
+        """
+
         json_data = request.get_json()
-        return recipes.update_recipe(id, json_data)
+        return recipes.update_recipe(recipe_id, json_data)
 
-    def delete(self, id: str):
-        return recipes.delete_recipe(id), 204
+    def delete(self, recipe_id: str):
+        """
+        Delete recipe by id
+        :param recipe_id: recipe_id uuid str
+        :return: 204 always
+        """
+
+        return recipes.delete_recipe(recipe_id), 204
 
 
 class RecipeRecommendation(Resource):
 
-    @rrqp.use_args(RecipesRecommendationsQueryParams(unknown=RAISE), location='query')
+    @rrqp.use_args(RecipesRecommendationsQueryParams(unknown=RAISE),
+                   location='query')
     def get(self, params: 'RecipesRecommendationsQueryParams'):
         print(type(params))
         print(params)
