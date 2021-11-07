04fe59c94e59bf0ff9c4a71b0854eafe889de961
ejsuarez793 <ejsuarez793@gmail.com>
2021-11-06 21:48:30 -0300

    testing port config

--- app/__main__.py
+++ app/__main__.py
@@ -1,8 +1,11 @@
+import os
 from app import create_app
 
 from app.server import app
 
 if __name__ == '__main__':
     create_app()
-    app.run(debug=True)
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
